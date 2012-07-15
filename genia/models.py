19bfcfe5e46257d20b34f257098259503d81c456
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-07-15 11:47:59 -0700

    Add initial model definition

--- genia/models.py
+++ genia/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Generation(models.Model):
+    app_name = models.CharField(max_length=255)
+    index = models.IntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(null=True)
+    current = models.BooleanField()
+    class Meta:
+        unique_together = ('app_name', 'index')
