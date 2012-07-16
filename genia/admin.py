b828e282a71aecb27c4cc0fa260c0af19d476d84
Taylor Mitchell <taylor.mitchell@gmail.com>
2012-07-15 17:35:21 -0700

    PEP-8 cleanup

--- genia/admin.py
+++ genia/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
-from models import Generation
+from genia.models import Generation
 
 
 class GenerationAdmin(admin.ModelAdmin):
-    list_display = ['app_name','index','created','last_updated','active']
+    """Admin for our Generation model"""
+    list_display = ['app_name', 'index', 'created', 'last_updated', 'active']
     list_filter = ['app_name',]
-    ordering = ['app_name','index']
+    ordering = ['app_name', 'index']
 admin.site.register(Generation, GenerationAdmin)
